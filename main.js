fa566d4a125d5c60abd550b69fc89f684e1129e0
alishadhar <alishadhar7@gmail.com>
2016-11-03 15:57:57 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+function math() {
+	var answer = prompt("Enter 1+1");
+  if (answer == 2) {
+  	confirm("You got it right.");
+  }
+  else {
+  confirm("You got it wrong.");
+  }
+}
+
+
+var m = 16;
+var s = 17;
+
+var m = prompt("5+4=");
+if (m == 9){
+	confirm("yay you got it right");
+}
+else {
+confirm("You suck at math.");
+}
